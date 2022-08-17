59f917f5d86be6c4199a2f636dde1d91f4497f35
Axel Leroy <3141536+axeleroy@users.noreply.github.com>
2022-08-17 22:39:11 +0200

    Replace Player class with mocks

--- flask/gamestate_tests/game.py
+++ flask/gamestate_tests/game.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 
 from gamestate.deck import Deck
 from gamestate.game import Game
@@ -17,42 +18,43 @@ class GameTestCase(unittest.TestCase):
     def test_add_player(self):
         uuid = '47b71b00-e060-47ba-8fae-029f5473794b'
         game = Game()
-        player = Player('Peter', True)
+        player = Mock()
         game.player_joins(uuid, player)
         self.assertTrue(uuid in game.list_players_uuid())
 
     def test_remove_player(self):
         uuid = '47b71b00-e060-47ba-8fae-029f5473794b'
         game = Game()
-        player = Player('Peter', True)
+        player = Mock()
         game.player_joins(uuid, player)
+        self.assertTrue(uuid in game.list_players_uuid())
         game.player_leaves(uuid)
         self.assertTrue(uuid not in game.list_players_uuid())
 
     def test_list_players(self):
         game = Game()
         uuid1 = '2c6f0ffa-ad57-47b7-bb34-2275d05018cf'
-        player1 = Player('John', True)
+        player1 = Mock()
         game.player_joins(uuid1, player1)
         uuid2 = '1499d88f-7841-47f1-a734-10a638bd4884'
-        player2 = Player('Peter', False)
+        player2 = Mock()
         game.player_joins(uuid2, player2)
         self.assertEqual(game.list_players(), [(uuid1, player1), (uuid2, player2)])
 
     def test_list_players_uuid(self):
         game = Game()
         uuid1 = '2c6f0ffa-ad57-47b7-bb34-2275d05018cf'
-        player1 = Player('John', True)
+        player1 = Mock()
         game.player_joins(uuid1, player1)
         uuid2 = '1499d88f-7841-47f1-a734-10a638bd4884'
-        player2 = Player('Peter', False)
+        player2 = Mock()
         game.player_joins(uuid2, player2)
         self.assertEqual(game.list_players_uuid(), [uuid1, uuid2])
 
     def test_get_player(self):
         game = Game()
         uuid1 = '2c6f0ffa-ad57-47b7-bb34-2275d05018cf'
-        player1 = Player('John', True)
+        player1 = Mock()
         game.player_joins(uuid1, player1)
         self.assertEqual(game.get_player(uuid1), player1)
 
@@ -65,39 +67,37 @@ class GameTestCase(unittest.TestCase):
         game = Game(Deck.POWERS)
         uuid1 = '2c6f0ffa-ad57-47b7-bb34-2275d05018cf'
         uuid2 = '0f6b59d5-6765-4be1-b196-760c654729a6'
-        player1 = Player('John', False)
-        game.player_joins(uuid1, player1)
+        player_mock = Mock()
+        game.player_joins(uuid1, player_mock)
         game.player_picks(uuid1, 8)
-        self.assertEqual(player1.get_hand(), 8)
+        player_mock.set_hand.assert_called_with(8)
 
         with self.assertRaises(IllegalOperationError) as ex1:
             game.player_picks(uuid2, 8)
         self.assertEqual(str(ex1.exception), f'Player with UUID {uuid2} is not in this game')
+
         with self.assertRaises(IllegalOperationError) as ex2:
             game.player_picks(uuid1, 13)
         self.assertEqual(str(ex2.exception), 'Card value 13 is not valid. Current deck is POWERS')
 
     def test_end_turn(self):
         game = Game()
-        player_1 = Player('John', False)
+        player_1 = Mock()
         game.player_joins('j', player_1)
-        player_2 = Player('Peter', False)
+        player_2 = Mock()
         game.player_joins('p', player_2)
-        spectator_1 = Player('Daisy', True)
+        spectator_1 = Mock()
         game.player_joins('d', spectator_1)
-        player_1.set_hand(8)
-        player_2.set_hand(13)
-        self.assertTrue(game.has_all_players_picked_card())
         game.end_turn()
-        self.assertFalse(game.has_all_players_picked_card())
-        self.assertEqual(player_1.get_hand(), None)
-        self.assertEqual(player_2.get_hand(), None)
+        player_1.clear_hand.assert_called()
+        player_2.clear_hand.assert_called()
+        spectator_1.clear_hand.assert_called()
 
     def test_is_game_empty(self):
         uuid = '47b71b00-e060-47ba-8fae-029f5473794b'
         game = Game()
         self.assertTrue(game.is_game_empty())
-        player = Player('Peter', True)
+        player = Mock()
         game.player_joins(uuid, player)
         self.assertFalse(game.is_game_empty())
         game.player_leaves(uuid)
@@ -105,58 +105,83 @@ class GameTestCase(unittest.TestCase):
 
     def test_get_non_spectator_players(self):
         game = Game()
-        player_1 = Player('John', False)
+        player_1 = Mock()
+        player1_conf = {'spectator': False}
+        player_1.configure_mock(**player1_conf)
         game.player_joins('j', player_1)
-        player_2 = Player('Peter', False)
+        player_2 = Mock()
+        player2_conf = {'spectator': False}
+        player_2.configure_mock(**player2_conf)
         game.player_joins('p', player_2)
-        spectator_1 = Player('Daisy', True)
+        spectator_1 = Mock()
+        spectator_conf = {'spectator': True}
+        spectator_1.configure_mock(**spectator_conf)
         game.player_joins('d', spectator_1)
         self.assertEqual(game.get_non_spectator_players(), [player_1, player_2])
 
     def test_all_players_have_picked_card(self):
         game = Game()
-        player_1 = Player('John', False)
+        player_1 = Mock()
+        player1_conf = {'spectator': False, 'has_picked_card.return_value': True}
+        player_1.configure_mock(**player1_conf)
         game.player_joins('j', player_1)
-        player_2 = Player('Peter', False)
+
+        player_2 = Mock()
+        player2_conf = {'spectator': False, 'has_picked_card.return_value': False}
+        player_2.configure_mock(**player2_conf)
         game.player_joins('p', player_2)
-        spectator_1 = Player('Daisy', True)
+
+        spectator_1 = Mock()
+        spectator_conf = {'spectator': True, 'has_picked_card.return_value': False}
+        spectator_1.configure_mock(**spectator_conf)
         game.player_joins('d', spectator_1)
         self.assertFalse(game.has_all_players_picked_card())
-        player_1.set_hand(8)
-        self.assertFalse(game.has_all_players_picked_card())
-        player_2.set_hand(13)
+
+        player2_conf['has_picked_card.return_value'] = True
+        player_2.configure_mock(**player2_conf)
         self.assertTrue(game.has_all_players_picked_card())
 
     def test_state(self):
         game = Game()
-        player_1 = Player('John', False)
-        player_1.set_hand(8)
+        player_1 = Mock()
+        player1_state = {'name': 'John', 'spectator': False, 'hasPicked': True}
+        player_1.configure_mock(**{'state.return_value': player1_state})
         game.player_joins('j', player_1)
-        player_2 = Player('Peter', False)
+        player_2 = Mock()
+        player2_state = {'name': 'Peter', 'spectator': False, 'hasPicked': False}
+        player_2.configure_mock(**{'state.return_value': player2_state})
         game.player_joins('p', player_2)
-        spectator_1 = Player('Daisy', True)
+        spectator_1 = Mock()
+        spectator1_state = {'name': 'Daisy', 'spectator': True, 'hasPicked': False}
+        spectator_1.configure_mock(**{'state.return_value': spectator1_state})
         game.player_joins('d', spectator_1)
 
         self.assertEqual(game.state(), [
-            ('j', {'name': 'John', 'spectator': False, 'hasPicked': True}),
-            ('p', {'name': 'Peter', 'spectator': False, 'hasPicked': False}),
-            ('d', {'name': 'Daisy', 'spectator': True, 'hasPicked': False}),
+            ('j', player1_state),
+            ('p', player2_state),
+            ('d', spectator1_state),
         ])
 
     def test_reveal_hand(self):
         game = Game()
-        player_1 = Player('John', False)
-        player_1.set_hand(8)
+        player_1 = Mock()
+        player1_state = {'spectator': False, 'get_hand.return_value': 8}
+        player_1.configure_mock(**player1_state)
         game.player_joins('j', player_1)
-        player_2 = Player('Peter', False)
+        player_2 = Mock()
+        player2_state = {'spectator': False, 'get_hand.return_value': None}
+        player_2.configure_mock(**player2_state)
         game.player_joins('p', player_2)
-        spectator_1 = Player('Daisy', True)
+        spectator_1 = Mock()
+        spectator1_state = {'spectator': True}
+        spectator_1.configure_mock(**spectator1_state)
         game.player_joins('d', spectator_1)
 
         self.assertEqual(game.reveal_hands(), [
             ('j', 8),
             ('p', None),
         ])
+        spectator_1.get_hand.assert_not_called()
 
 
 if __name__ == '__main__':

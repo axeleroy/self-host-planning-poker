0171ebb4bc216a5263c3c257f2f032921b0ba5f3
Axel Leroy <3141536+axeleroy@users.noreply.github.com>
2022-09-01 22:08:43 +0200

    The action to join a game is now a separate event

--- flask/app.py
+++ flask/app.py
@@ -1,30 +1,44 @@
 from flask import Flask, request, session
-from flask_socketio import SocketIO, join_room, send, leave_room
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from peewee import SqliteDatabase
 
+from gamestate.deck import Deck
+from gamestate.game import Game
 from gamestate.game_manager import GameManager
-
-gm = GameManager()
+from gamestate.models import database_proxy, StoredGame
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, logger=True, engineio_logger=True)
 
-if __name__ == '__main__':
-    socketio.run(app)
+if app.config['DEBUG'] or app.config['TESTING']:
+    real_db = SqliteDatabase(':memory:')
+else:
+    real_db = SqliteDatabase('/app/database.db')
+database_proxy.initialize(real_db)
+if database_proxy.is_closed():
+    database_proxy.connect()
+StoredGame.create_table()
+
+gm = GameManager()
+
+# For testing purposes only
+gm.games['8b70cb3d-00ba-4fcc-aaac-60f699d4170f'] = Game('PBR Pizza', Deck.POWERS)
 
 
-@socketio.on('connect')
-def handle_connect(data):
+@socketio.on('join')
+def handle_join(data):
     player_id = request.sid
     player_name = data['name']
     spectator = data['spectator']
-
     game_id = data['game']
+
     session['game_id'] = game_id
     join_room(game_id)
 
-    state = gm.join_game(game_id, player_id, player_name, spectator)
-    send(state, to=game_id)
+    info, state = gm.join_game(game_id, player_id, player_name, spectator)
+    emit('state', state, to=game_id, json=True)
+    return info
 
 
 @socketio.on('disconnect')
@@ -34,4 +48,8 @@ def hand_disconnect():
 
     state = gm.leave_game(game_id, player_id)
     leave_room(game_id)
-    send(state, to=game_id)
+    emit('state', state, to=game_id, json=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)

cce3ebc23a0442a9a0ec2869ea25ef144d809cc9
Axel Leroy <3141536+axeleroy@users.noreply.github.com>
2022-09-01 22:29:42 +0200

    Use shorthand event handlers and handle most game events

--- flask/app.py
+++ flask/app.py
@@ -26,8 +26,8 @@ gm = GameManager()
 gm.games['8b70cb3d-00ba-4fcc-aaac-60f699d4170f'] = Game('PBR Pizza', Deck.POWERS)
 
 
-@socketio.on('join')
-def handle_join(data):
+@socketio.event
+def join(data):
     player_id = request.sid
     player_name = data['name']
     spectator = data['spectator']
@@ -41,13 +41,59 @@ def handle_join(data):
     return info
 
 
-@socketio.on('disconnect')
-def hand_disconnect():
+@socketio.event
+def disconnect():
     player_id = request.sid
     game_id = session['game_id']
 
     state = gm.leave_game(game_id, player_id)
     leave_room(game_id)
+    emit('state', state, to=game_id, json=True)
+
+
+@socketio.event
+def set_player_name(data):
+    player_id = request.sid
+    game_id = session['game_id']
+    player_name = data['name']
+
+    state = gm.set_player_name(game_id, player_id, player_name)
+    emit('state', state, to=game_id, json=True)
+
+
+@socketio.event
+def set_spectator(data):
+    player_id = request.sid
+    game_id = session['game_id']
+    is_specatator = data['spectator']
+
+    state = gm.set_player_spectator(game_id, player_id, is_specatator)
+    emit('state', state, to=game_id, json=True)
+
+
+@socketio.event
+def pick_card(data):
+    player_id = request.sid
+    game_id = session['game_id']
+    card = data['card']
+
+    state = gm.pick_card(game_id, player_id, card)
+    emit('state', state, to=game_id, json=True)
+
+
+@socketio.event
+def reveal_cards(data):
+    game_id = session['game_id']
+
+    hands = gm.reveal_cards(game_id)
+    emit('hands', hands, to=game_id, json=True)
+
+
+@socketio.event
+def end_turn(data):
+    game_id = session['game_id']
+
+    state = gm.end_turn(game_id)
     emit('state', state, to=game_id, json=True)
 
 

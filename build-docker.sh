99edcfcd04aeb5fb4c75be0a8889dcd9069f7978
Axel Leroy <axeleroy@users.noreply.github.com>
2022-09-13 21:57:14 +0200

    Fix docker image build and add multi-arch

--- build-docker.sh
+++ build-docker.sh
@@ -2,9 +2,9 @@
 
 cd angular
 npm i
-ng build self-host-planning-poker -c production
+npm run build self-host-planning-poker
+mkdir -p ../flask/static
 cp -R dist/self-host-planning-poker/assets/* ../flask/static/
 cp dist/self-host-planning-poker/*.* ../flask/static
 cd ..
-docker build -t self-host-planning-poker flask
-
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag axeleroy/self-host-planning-poker:latest flask

fe9745dc8290857f38ce26273ed9c37af62a48dd
Axel Leroy <3141536+axeleroy@users.noreply.github.com>
2022-09-19 15:32:50 +0200

    Trust vote only goes from 0 to 5

--- flask/gamestate/deck.py
+++ flask/gamestate/deck.py
@@ -6,5 +6,5 @@ class Deck(Enum):
     FIBONACCI = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     MODIFIED_FIBONACCI = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100]
     POWERS = [0, 1, 2, 4, 8, 16, 32, 64]
-    TRUST_VOTE = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    TRUST_VOTE = [0, 1, 2, 3, 4, 5]
     T_SHIRTS = [1, 2, 3, 4, 5, 6, 7]  # Each value maps to a t-shirt size from XXS to XXL

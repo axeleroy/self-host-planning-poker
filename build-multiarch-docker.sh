c22f4a1d3b31771b67e8339a81780aa2bc7efcf6
Axel Leroy <3141536+axeleroy@users.noreply.github.com>
2022-09-20 16:02:29 +0200

    Fix docker buildx

--- build-multiarch-docker.sh
+++ build-multiarch-docker.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
-
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx create --driver docker-container --use
+docker buildx inspect --bootstrap
 docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag axeleroy/self-host-planning-poker:latest .

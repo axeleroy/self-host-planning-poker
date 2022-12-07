124be6b8cd868bc87f821a575aedab5d3313f881
Axel Leroy <axeleroy@users.noreply.github.com>
2022-12-07 22:35:16 +0100

    Add button to copy game link to clipboard #2

--- angular/src/app/components/navigation-bar/game-name/game-name.component.ts
+++ angular/src/app/components/navigation-bar/game-name/game-name.component.ts
@@ -2,6 +2,9 @@ import { Component, OnDestroy } from '@angular/core';
 import { CurrentGameService } from '../../../services/current-game.service';
 import { GameInfo } from '../../../model/events';
 import { Subscription } from 'rxjs';
+import { ToastService } from '../../../services/toast/toast.service';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'shpp-game-name',
@@ -14,13 +17,23 @@ export class GameNameComponent implements OnDestroy {
   currentGameInfo: GameInfo | null | undefined;
   private gameInfoSubscription: Subscription;
 
-  constructor(private currentGameService: CurrentGameService) {
+  constructor(private currentGameService: CurrentGameService,
+              private toastService:  ToastService,
+              private clipboard: Clipboard,
+              private transloco: TranslocoService) {
     this.gameInfoSubscription = this.currentGameService.gameInfo$
     .subscribe((gameInfo) => this.currentGameInfo = gameInfo);
   }
 
   ngOnDestroy(): void {
     this.gameInfoSubscription.unsubscribe();
+  }
+
+  copyLink(): void {
+    this.clipboard.copy(window.location.toString());
+    this.toastService.show(
+      this.transloco.translate('navbar.gameName.copy-toast')
+    )
   }
 
 }

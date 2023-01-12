cb3b4c3898995ae36a8599b7ace75c21035a9eae
Axel Leroy <3141536+axeleroy@users.noreply.github.com>
2023-01-12 14:39:06 +0100

    angular(#10): Safelist classes added programmatically through ngBootstrap

--- angular/webpack.config.js
+++ angular/webpack.config.js
@@ -4,7 +4,24 @@ const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
 module.exports = {
   plugins: [
     new PurgeCSSPlugin({
-      paths: glob.sync('./src/**/*.html', { nodir: true })
+      paths: glob.sync('./src/**/*.html', { nodir: true }),
+      safelist: {
+        deep: [
+          /offcanvas-/,
+          /tooltip-/,
+          /tooltip/,
+          // Toast
+          /toast-/,
+          /toast/,
+          /fade/,
+          /end-/,
+          /position-/,
+          /p-/,
+          /top-/,
+          /bg-/,
+          /text-/
+        ]
+      }
     })
   ]
 };

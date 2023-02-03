27e190a9b17993a9ffe0cd2afa310cc7608e90d2
Axel Leroy <3141536+axeleroy@users.noreply.github.com>
2023-02-03 17:45:20 +0100

    feat(angular): updated design and branding (#13)

--- angular/src/app/components/navigation-bar/navigation-bar.component.ts
+++ angular/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'shpp-navigation-bar',
-  templateUrl: './navigation-bar.component.html'
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {
 

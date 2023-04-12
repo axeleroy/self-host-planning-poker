56c4efa12734bb6177c7a62d4685807ed8050db7
Axel Leroy <axeleroy@users.noreply.github.com>
2023-04-12 23:57:00 +0200

    feat(angular): add confettis on 100% agreement

--- angular/src/app/ongoing-game/turn-summary/turn-summary.component.ts
+++ angular/src/app/ongoing-game/turn-summary/turn-summary.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { PlayerState } from '../../model/events';
-import { combineLatest, filter, map, Subscription, tap } from 'rxjs';
+import { combineLatest, debounceTime, filter, map, Observable, Subscription, tap } from 'rxjs';
 import { Deck, decksDict, displayCardValue } from '../../model/deck';
 import { KeyValue } from '@angular/common';
 import { CurrentGameService } from '../current-game.service';
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'shpp-turn-summary',
   templateUrl: './turn-summary.component.html',
   styleUrls: [ './turn-summary.component.scss' ]
 })
-export class TurnSummaryComponent implements OnDestroy {
-  private subscription: Subscription;
+export class TurnSummaryComponent implements AfterViewInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
 
   displayCardValue = displayCardValue;
   Number = Number;
@@ -21,11 +22,17 @@ export class TurnSummaryComponent implements OnDestroy {
 
   deck: Deck = decksDict['FIBONACCI'];
   average = 0;
-  counts: CardCount = {};
   agreement = 0;
+  $counts: Observable<CardCount>;
+  private $playerStates: Observable<PlayerState[]>;
+  private $agreement: Observable<number>;
+
+  @ViewChild('agreementElement')
+  private agreementElement?: ElementRef;
 
   constructor(private currentGameService: CurrentGameService) {
-    this.subscription = combineLatest([this.currentGameService.state$, this.currentGameService.gameInfo$])
+    // FIXME: one ofthe two obserables are triggered multiple times after initial reveal
+    this.$playerStates = combineLatest([this.currentGameService.state$, this.currentGameService.gameInfo$])
     .pipe(
       filter(([, gameInfo]) => gameInfo !== null && gameInfo.revealed),
       tap(([, gameInfo]) => {
@@ -33,18 +40,40 @@ export class TurnSummaryComponent implements OnDestroy {
           this.deck = decksDict[gameInfo.deck];
         }
       }),
-      map(([gameState]) => Object.values(gameState))
-    ).subscribe((playerStates: PlayerState[]) => {
-      const players = playerStates.filter((state) => state.hand !== undefined && state.hand !== null);
-      this.average = players.reduce((prev, current) => prev + (current.hand || 0) , 0) / players.length || 0;
-      this.counts = players.map((player) => player.hand || 0)
-        .reduce((previous, current) => {
-          let num = previous[current.toString()] || 0;
-          previous[current.toString()] = num + 1;
-          return previous;
-        }, {} as CardCount);
-      this.agreement = Math.max(0, ...Object.values(this.counts)) / players.length || 0;
-    });
+      map(([gameState]) => Object.values(gameState)),
+      map((playerStates: PlayerState[]) => playerStates.filter((state) => state.hand !== undefined && state.hand !== null))
+    );
+
+    this.$counts = this.$playerStates
+    .pipe(map((players: PlayerState[]) =>
+      players
+      .map((player) => player.hand || 0)
+      .reduce((previous, current) => {
+        let num = previous.get(current.toString()) || 0;
+        previous.set(current.toString(), num + 1);
+        return previous;
+      }, new Map() as CardCount)
+    ));
+
+    this.$agreement = this.$counts
+    .pipe(map((counts) => (Math.max(0, ...counts.values()) / counts.size || 0)));
+
+    this.subscriptions.concat(
+      this.$playerStates.pipe(
+        map((players: PlayerState[]) =>
+          players.reduce((prev, current) => prev + (current.hand || 0), 0) / players.length || 0))
+      .subscribe((value) => this.average = value));
+
+    this.subscriptions.concat(
+      this.$agreement
+      .subscribe((value) => this.agreement = value));
+  }
+
+  ngAfterViewInit(): void {
+    this.subscriptions.concat(
+      this.$agreement
+      .pipe(filter((value) => value === 1))
+      .subscribe(() => this.fireConfettis()));
   }
 
   agreementClass(): string {
@@ -60,9 +89,51 @@ export class TurnSummaryComponent implements OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.forEach((s) => s.unsubscribe());
   }
 
+  private fireConfettis() {
+    const domRect = this.agreementElement?.nativeElement.getBoundingClientRect();
+    const x = (domRect.left + domRect.width / 2) / window.innerWidth;
+    const y = (domRect.top + domRect.height / 2) / window.innerHeight;
+    const origin = { x: x, y: y };
+    console.log(origin)
+    this.fireParticles(0.25, {
+      origin: origin,
+      spread: 26,
+      startVelocity: 55,
+    });
+    this.fireParticles(0.2, {
+      origin: origin,
+      spread: 60,
+    });
+    this.fireParticles(0.35, {
+      origin: origin,
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+    this.fireParticles(0.1, {
+      origin: origin,
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+    this.fireParticles(0.1, {
+      origin: origin,
+      spread: 120,
+      startVelocity: 45,
+    });
+  }
+
+  private fireParticles(particleRatio: number, opts: any) {
+    confetti({
+      ...opts,
+      disableForReducedMotion: true,
+      particleCount: Math.floor(200 * particleRatio)
+    });
+  }
 }
 
-type CardCount = Record<string, number>;
+type CardCount = Map<string, number>;

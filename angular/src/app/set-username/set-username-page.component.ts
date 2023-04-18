96a1bf089cf067d5dfca491c3569ef78a0a82c87
Axel Leroy <axeleroy@users.noreply.github.com>
2023-04-18 22:42:26 +0200

    chore(angular): Improve navbar

--- angular/src/app/set-username/set-username-page.component.ts
+++ angular/src/app/set-username/set-username-page.component.ts
@@ -3,12 +3,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { TranslocoRootModule } from '../transloco-root.module';
 import { ContainerComponent } from '../shared/container/container.component';
 import { PlayerNameFormComponent } from '../player-info/player-name-form/player-name-form.component';
+import { NavAppTitleComponent } from '../shared/navigation-bar/app-title/nav-app-title.component';
 
 @Component({
   standalone: true,
   selector: 'shpp-set-username-page',
   templateUrl: './set-username-page.component.html',
-  imports: [ PlayerNameFormComponent, TranslocoRootModule, ContainerComponent ]
+  imports: [PlayerNameFormComponent, TranslocoRootModule, ContainerComponent, NavAppTitleComponent]
 })
 export class SetUsernamePageComponent implements OnInit {
   gameId?: string;
---
+++ angular/src/app/shared/navigation-bar/app-title/nav-app-title.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslocoRootModule } from '../../../transloco-root.module';
+
+@Component({
+  selector: 'shpp-nav-app-title',
+  templateUrl: './nav-app-title.component.html',
+  standalone: true,
+  imports: [ TranslocoRootModule ]
+})
+export class NavAppTitleComponent {
+
+}

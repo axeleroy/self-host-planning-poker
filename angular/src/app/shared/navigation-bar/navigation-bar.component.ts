f531b11f4720d111a3d6e830e1a90856aafbf857
Axel Leroy <axeleroy@users.noreply.github.com>
2023-04-18 22:34:59 +0200

    feat(angular): Link to homepage in navbar

--- angular/src/app/shared/navigation-bar/navigation-bar.component.ts
+++ angular/src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslocoRootModule } from '../../transloco-root.module';
 
 @Component({
   standalone: true,
   selector: 'shpp-navigation-bar',
   templateUrl: './navigation-bar.component.html',
+  imports: [
+    TranslocoRootModule
+  ],
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {

b0e938169ab576b2aee257784b58037cfcea2629
Axel Leroy <3141536+axeleroy@users.noreply.github.com>
2023-04-20 14:41:10 +0200

    fix(angular): unused import

--- angular/src/app/shared/container/container.component.ts
+++ angular/src/app/shared/container/container.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ToastsContainerComponent } from '../toast/toast-container.component';
 import { NavigationBarComponent } from '../../navigation-bar/navigation-bar.component';
 
 @Component({

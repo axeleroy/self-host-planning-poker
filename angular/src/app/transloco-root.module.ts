39d35592e372bbc2aded0b6681b1160d9cc02ab6
Axel Leroy <axeleroy@users.noreply.github.com>
2023-06-13 22:23:28 +0200

    fix(angular): Transloco would not fallback when a lang is not defined in the availableLangs array but has a translation file. Closes #93

--- angular/src/app/transloco-root.module.ts
+++ angular/src/app/transloco-root.module.ts
@@ -34,7 +34,8 @@ export class TranslocoHttpLoader implements TranslocoLoader {
     {
       provide: TRANSLOCO_CONFIG,
       useValue: translocoConfig({
-        availableLangs: ['en', 'fr'],
+        availableLangs: ['af', 'ar', 'ca', 'cs', 'da', 'de', 'el', 'en', 'es', 'fi', 'fr', 'he', 'hu', 'it', 'ja', 'ko',
+          'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'sr', 'sv', 'tr', 'uk', 'vi', 'zh'],
         fallbackLang: 'en',
         prodMode: environment.production,
         missingHandler: {

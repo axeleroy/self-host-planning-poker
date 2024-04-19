1284fc70e5f027696430c403536e3e2e8662dad6
Gopikrishna Sathyamurthy <Gopikrishna19@users.noreply.github.com>
2024-04-19 16:41:24 -0500

    feat: add confidence votes deck
    As part of our object/key results we vote on confidence in finishing the KR by the end of a quarter. And we vote it from 0-10

--- angular/src/app/model/deck.ts
+++ angular/src/app/model/deck.ts
@@ -71,6 +71,23 @@ export const decks: Deck[] = [
     ]
   },
   {
+    name: 'CONFIDENCE_VOTES',
+    textValues: false,
+    values: [
+      { value: 0, display: 0 },
+      { value: 1, display: 1 },
+      { value: 2, display: 2 },
+      { value: 3, display: 3 },
+      { value: 4, display: 4 },
+      { value: 5, display: 5 },
+      { value: 6, display: 6 },
+      { value: 7, display: 7 },
+      { value: 8, display: 8 },
+      { value: 9, display: 9 },
+      { value: 10, display: 10 },
+    ]
+  },
+  {
     name: 'T_SHIRTS',
     textValues: true,
     values: [
